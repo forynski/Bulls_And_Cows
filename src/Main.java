1deb5a67dff548efc802d7bce525b621a3d2c847
forynski <65871510+forynski@users.noreply.github.com>
2021-02-12 13:35:21 +0000

    Stage 5/7 implemented shuffle code method

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Please, enter the secret code's length:");
+        System.out.println("Input the length of the secret code:");
         Scanner scanner = new Scanner(System.in);
         int sc = scanner.nextInt();
-        if (sc > 10) {
-            System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
+        if (sc > 36) {
+            System.out.println("Error: can't generate a secret number with a length of 36 because there aren't enough unique symbols.");
         } else {
 
             String secretCode = generateSecretCode(sc);
@@ -19,23 +19,50 @@ public class Main {
 
     static String generateSecretCode(int sc) {
 
-        Random random = new Random();
-        int randomNumber = Math.abs(random.nextInt());
-        String s2 = Integer.toString(randomNumber);
 
-        char[] chars = s2.toCharArray();
-        Set<Character> characterSet = new LinkedHashSet<>();
-        for (char c : chars) {
-            characterSet.add(c);
-        }
+        System.out.println("Input the number of possible symbols in the code:");
+        Scanner scanner = new Scanner(System.in);
+        scanner.hasNextLine();
 
-        StringBuilder sb = new StringBuilder();
-        for (Character character : characterSet) {
-            sb.append(character);
-        }
+        String chars = "0123456789abcdefghijklmnopqrstuvwxyz";
 
-        return sb.substring(0, sc);
+//        Random random = new Random();
+
+//        int randomNumber = Math.abs(random.nextInt());
+//        String s2 = Integer.toString(randomNumber);
+
+//        char[] chars = s2.toCharArray();
+//        Set<Character> characterSet = new LinkedHashSet<>();
+//        for (char c : chars) {
+//            characterSet.add(c);
+//        }
+
+        StringBuilder sb = new StringBuilder(sc);
+        for (int i = 0; i < sc; i++)
+            sb.append(chars.charAt(i));
+
+//        shuffle(sb);
+
+//        String secret = sb.toString().replaceAll("[a-z0-9]+$", "*");
+//
+//        System.out.println("The secret is prepared: " + secret + " (0-9, a-f).");
+
+        return shuffle(sb);
+//        return sb.toString();
+//        return sb.substring(0, sc);
     }
+
+    private static String shuffle(StringBuilder sb) {
+        char[] characters = sb.toString().toCharArray();
+        for (int i = 0; i < characters.length; i++) {
+            int randomIndex = (int) (Math.random() * characters.length);
+            char temp = characters[i];
+            characters[i] = characters[randomIndex];
+            characters[randomIndex] = temp;
+        }
+        return new String(characters);
+    }
+
 
     private static void calculateCowsAndBulls(String secretCode, int sc) {
 
@@ -76,3 +103,5 @@ public class Main {
     }
 
 }
+
+
